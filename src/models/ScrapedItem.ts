3c948c3d3148823505a36742758e352fdc9aa78d
veselv2010 <veselv2010@gmail.com>
2022-09-08 14:20:39 +0300

    item model

---
+++ src/models/ScrapedItem.ts
@@ -0,0 +1,15 @@
+class ScraptedItem {
+    readonly name: String;
+    readonly price: number;
+    readonly url: String;
+
+    constructor(name: String, price: number, url: String) {
+        this.name = name;
+        this.price = price;
+        this.url = url;
+    }
+
+    getPrice(): String {
+        return `${this.price} ₽`;
+    }
+}
