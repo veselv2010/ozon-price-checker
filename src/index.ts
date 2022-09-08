7f668c253f42310edec5836f972fef8efdec7706
ysur67 <rus.muratow@gmail.com>
2022-09-08 16:02:27 +0300

    добавлена камера газовая

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
-import { Context, Telegraf } from 'telegraf';
-import { Update } from 'typegram';
-import fs = require('fs');
+import { Context, Telegraf } from "telegraf";
+import { Update } from "typegram";
+import * as puppeteer from "puppeteer";
+import fs = require("fs");
+import { ScraptedItem } from "./models/ScrapedItem";
 
-const cachePath = '.cache/cache.json';
-const bot: Telegraf<Context<Update>> = new Telegraf(process.env.BOT_KEY as string);
+const cachePath = ".cache/cache.json";
+const bot: Telegraf<Context<Update>> = new Telegraf(
+    process.env.BOT_KEY as string
+);
 
 bot.start((ctx) => {
-    ctx.reply('Hello ' + ctx.from.first_name + '!');
+    ctx.reply("Hello " + ctx.from.first_name + "!");
     saveChatIdToCache(ctx.message.chat.id.toString());
 });
 
-bot.command('quit', (ctx) => {
+bot.command("quit", (ctx) => {
     ctx.leaveChat();
-    removeChatIdFromCache(ctx.message.chat.id.toString())
+    removeChatIdFromCache(ctx.message.chat.id.toString());
 });
 
 function sendMessage(info: ScraptedItem[]): void {
     const chatIds = getCachedChatIds();
-    const composedMessages = [...Array(info.length).keys()].map<string>((index) => {
-        const item = info.at(index);
-        const message = `${index + 1}. ${composeMessage(item)}`;
-        return `[${message}](${item.url})`;
-    }).join('\n');
+    const composedMessages = [...Array(info.length).keys()]
+        .map<string>((index) => {
+            const item = info.at(index);
+            const message = `${index + 1}. ${composeMessage(item)}`;
+            return `[${message}](${item.url})`;
+        })
+        .join("\n");
     chatIds.forEach((id) => {
         bot.telegram.sendMessage(id, composedMessages);
     });
@@ -34,9 +40,8 @@ function composeMessage(item: ScraptedItem): string {
 function getCachedChatIds(): string[] {
     try {
         return getCacheArray();
-    }
-    catch (err) {
-        console.error(err)
+    } catch (err) {
+        console.error(err);
         return [];
     }
 }
@@ -46,8 +51,7 @@ function removeChatIdFromCache(id: string): void {
         const json = getCacheArray();
         delete json[json.indexOf(id)];
         saveCacheArray(json);
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err);
     }
 }
@@ -57,19 +61,48 @@ function saveChatIdToCache(id: string): void {
         const json = getCacheArray();
         json.push(id);
         saveCacheArray(json);
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err);
     }
 }
 
 function getCacheArray(): Array<string> {
-    const file = fs.readFileSync(cachePath, 'utf8');
+    const file = fs.readFileSync(cachePath, "utf8");
     return JSON.parse(file) as Array<string>;
-
 }
 
 function saveCacheArray(cache: Array<string>): void {
-    fs.writeFileSync(cachePath, JSON.stringify(cache), 'utf-8');
+    fs.writeFileSync(cachePath, JSON.stringify(cache), "utf-8");
 }
 
+const URL =
+    "https://www.ozon.ru/category/videokarty-15721/?deny_category_prediction=true&from_global=true&sorting=ozon_card_price&text=rtx+3080";
+
+bot.start((ctx) => {
+    ctx.reply("Hello " + ctx.from.first_name + "!");
+});
+
+bot.command("quit", (ctx) => {
+    // Explicit usage
+    ctx.telegram.leaveChat(ctx.message.chat.id); // Context shortcut
+    ctx.leaveChat();
+});
+
+const main = async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    const response = await page.goto(URL);
+    const dimensions = await page.evaluate(() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio,
+        };
+    });
+
+    console.log("Dimensions:", dimensions);
+
+    await browser.close();
+};
+
+main();
