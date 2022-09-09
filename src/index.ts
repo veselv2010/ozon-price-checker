85a5ce4cde0ee4fdc297d676bb8aa3b72914a638
ysur67 <rus.muratow@gmail.com>
2022-09-09 18:30:57 +0300

    remove redundant comments

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
-import * as dotenv from "dotenv";
-import { OzonBot } from "./OzonBot";
-import { BotSubscribersCachedSet } from "./BotSubscribersCachedSet";
-import { OzonScraper } from "./OzonScraper";
+import * as dotenv from 'dotenv';
+import { OzonBot } from './OzonBot';
+import { BotSubscribersCachedSet } from './BotSubscribersCachedSet';
+import { OzonScraper } from './OzonScraper';
 
 dotenv.config();
 
 const OZON_SEARCH_RESULTS_URL =
-    "https://www.ozon.ru/category/videokarty-15721/?deny_category_prediction=true&from_global=true&sorting=ozon_card_price&text=rtx+3080";
+    'https://www.ozon.ru/category/videokarty-15721/?deny_category_prediction=true&from_global=true&sorting=ozon_card_price&text=rtx+3080';
 
-// const ozonBot = new OzonBot(
-//     process.env.BOT_KEY as string,
-//     new BotSubscribersCachedSet("./.cache/cache.json"),
-//     new OzonScraper(OZON_SEARCH_RESULTS_URL)
-// );
+const ozonBot = new OzonBot(
+    process.env.BOT_KEY as string,
+    new BotSubscribersCachedSet('./.cache/cache.json'),
+    new OzonScraper(OZON_SEARCH_RESULTS_URL)
+);
 
 const main = async () => {
-    const scrapper = new OzonScraper(OZON_SEARCH_RESULTS_URL);
-    const data = await scrapper.load(10);
-    console.log(data);
-    // await ozonBot.launch(1800000);
+    await ozonBot.launch(1800000);
 };
 
 main();
